938994078cacecf9917fa77e921d69c9a670ee9d
syntakks <syntakks@gmail.com>
2020-01-22 21:01:25 -0600

    Initial Commit
    First Commit

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10, 10, 10);
+    [SerializeField] float period = 2f;
+    //[Range(0,1)] [SerializeField] << This creates a range slider that can be manipulated within the inspector. 
+    float movementFactor; // 0 for not moved, 1 for fully moved. 
+    private Vector3 startingPosition; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; } // You can not compare floats as they may be off my tiny fractions of the number. We need to determine our Threshold of acceptable values in comparison. Think peoples height. 
+        float cycles = Time.time / period; // grows continually from 0
+        //print(cycles); 
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau); // gives a value from -1 to 1
+        //print(rawSinWave);
+        movementFactor = rawSinWave / 2f + 0.5f; // We need a value of 0-1
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset; 
+    }
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Rocket : MonoBehaviour
+{
+    //TODO create an array of available levels to traverse. 
+    //TODO create an var for current level.  
+    private enum State { Alive, Dying, Transcending }
+    private State state = State.Alive; 
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+    [SerializeField] private float rcsThrust = 250f; 
+    [SerializeField] private float mainThrust = 25f;
+    [SerializeField] private float levelLoadDelay = 2f; 
+    [SerializeField] private AudioClip thruster;
+    [SerializeField] private AudioClip death;
+    [SerializeField] private AudioClip success;
+    [SerializeField] private ParticleSystem thrustParticle;
+    [SerializeField] private ParticleSystem deathParticle;
+    [SerializeField] private ParticleSystem successParticle;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>(); 
+    }
+    
+    void FixedUpdate()
+    {
+        if (state == State.Alive)
+        {
+            CheckThrust();
+            CheckRotate();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) { return; }
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // Do nothing. 
+                break;
+            case "Finish":
+                TranscendLevel(); 
+                break;
+            default:
+                PlayerDied(); 
+                break; 
+        }   
+    }
+
+    private void TranscendLevel()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticle.Play(); 
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void PlayerDied()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticle.Play(); 
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void CheckThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Thrust
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(thruster);
+            }
+            thrustParticle.Play(); 
+        }
+        else
+        {
+            audioSource.Stop();
+            thrustParticle.Stop(); 
+        }
+    }
+
+    private void CheckRotate()
+    {
+        rigidBody.freezeRotation = true; 
+        if (Input.GetKey(KeyCode.A)) {
+            // Rotate Left
+            transform.Rotate(Vector3.forward * (rcsThrust * Time.deltaTime)); 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Rotate Right
+            transform.Rotate(-Vector3.forward * (rcsThrust * Time.deltaTime)); 
+        }
+        rigidBody.freezeRotation = false; 
+    }
+
+    
+}
